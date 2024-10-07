921e10c5df9d166d78fdaaf5990b010bb968d0f2
1DavidX <joseayala135@hotmail.com>
2024-10-07 08:43:58 -0500

    Ejercicio: De JavaScript

---
+++ Js/App.js
@@ -0,0 +1,5 @@
+var edad =prompt("Ingresar tu edad")
+var nombre =prompt("Ingrese su nombre")
+console.log("Hola, bienvenidos a todos a la ciudad de: Quito"); alert("Gracias por preferirnos");
+console.log("Tu edad es:" + edad +  "\n Bienvenido a Quito "  + nombre + "Al pasajero")
+alert(" Gracias por viajar, con nosotros señor/señora " + nombre + " \n Destino Quito " + edad + " de años de edad. ")
